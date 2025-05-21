7f495658800135bbb1b2cd6b3c9a7d12a32b3eb5
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-05-21 13:29:03 -0300

    AppContacts

--- App2/src/pages/Contacts/index.js
+++ App2/src/pages/Contacts/index.js
@@ -1,13 +1,35 @@
 import React from "react"
 import { View, Text } from "react-native"
 
-export default function Contacts({navigation}){
-    return(
+export default function Contacts({ navigation }) {
+    return (
         <View>
-            <Text>Vicente</Text>
-            <Text
-            onPress={()=> navigation.navigate('Information')}
-            >Informação...</Text>
+            <View>
+                <Text>Vicente</Text>
+                <Text>Telefone: (89) 98473-9585</Text>
+                <Text
+                    onPress={() => navigation.navigate('Information',
+                        {
+                            nome: 'Vicente Junior',
+                            telefone: '(89) 98473-9585',
+                            endereco: 'Rua 2',
+                            numero: '34',
+                            email: 'vicente@gmail.com'
+                        }
+                    )}
+                >Informação...</Text>
+            </View>
+            <View>
+                <Text>Davi</Text>
+                <Text>Telefone: (89) 98983-950985</Text>
+                <Text
+                    onPress={() => navigation.navigate('Information')}
+                >Informação...</Text>
+            </View>
+
         </View>
+
+
+
     )
 }
--- App2/src/pages/Information/index.js
+++ App2/src/pages/Information/index.js
@@ -1,10 +1,15 @@
 import React  from "react";
 import { View, Text } from "react-native";
 
-export default function Information(){
+export default function Information({ route }){
     return(
         <View>
-            <Text>Information</Text>
+            <Text>Informações dos contatos</Text>
+            <Text>Nome: {route.params?.nome} </Text>
+            <Text>Telefone: {route.params?.telefone} </Text>
+            <Text>Endereço: {route.params?.endereco} </Text>
+            <Text>N°: {route.params?.numero} </Text>
+            <Text>Email: {route.params?.email} </Text>
         </View>
     )
 }
